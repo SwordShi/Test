cfb69926c9912022283e782ccc73e7612065dc0e
shijian <shijian@cictec.com>
2016-12-05 11:26:23 +0800

    测试提交

---
+++ test.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script src="http://cdn.static.runoob.com/libs/angular.js/1.4.6/angular.min.js"></script>
+</head>
+<body>
+<div ng-app="myApp" ng-controller="myCtrl">
+
+<input type="number" ng-model="num"/>
+
+<h1>16进制：{{hex}}</h1>
+
+</div>
+
+<p>自定义服务，用于转换16进制数：</p>
+
+<script>
+/*var app = angular.module('myApp', []);
+
+app.service('hexafy', function() {
+	this.myFunc = function (x) {
+        return x.toString(16);
+    }
+});
+app.controller('myCtrl', function($scope, hexafy) {
+  $scope.hex = hexafy.myFunc(255);
+});*/
+
+//自定义服务，来进行进制转换
+var model = angular.module('myApp',[]);
+model.service('hexafy', function(){
+	//服务下的方法
+	this.myfunction = function(x){
+		return x.toString(16);
+	};
+});
+
+model.controller('myCtrl',function($scope , hexafy){
+	$scope.num = 15;
+	$scope.$watch('num',function(data){
+	  $scope.hex = hexafy.myfunction(data);
+	});
+	
+});
+	
+</script>
+
+</body>
+</html>
